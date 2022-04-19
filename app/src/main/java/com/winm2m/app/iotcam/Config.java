533c36cb2f987af4b3a4e53fad02f7ed6c2aec42
swoos91 <mb910805@gmail.com>
2022-04-19 16:24:43 +0900

    config save & load 기능 추가

--- app/src/main/java/com/winm2m/app/iotcam/Config.java
+++ app/src/main/java/com/winm2m/app/iotcam/Config.java
@@ -1,5 +1,16 @@
 package com.winm2m.app.iotcam;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Config {
     String mqttHost;
     String mqttPort;
@@ -12,6 +23,7 @@ public class Config {
     final static String DEFAULT_MQTT_HOST = "hub.winm2m.com";
     final static String DEFAULT_MQTT_PORT = "46083";
     final static String DEFAULT_SERIAL = "2ic00000";
+    final static String CONFIG_PATH = "/data/data/com.winm2m.app.iotcam/config";
 
 
     public String getMqttPort() {
@@ -81,10 +93,46 @@ public class Config {
 
     /* TODO : develop */
     public static Config load() {
-        return new Config().setDefault();
+        Config c = new Config().setDefault();
+        if (!new File(CONFIG_PATH).exists()) c.save();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(CONFIG_PATH));
+            Map<String, String> m = new HashMap<>();
+            for(String line=""; (line=reader.readLine())!=null && line.trim().length() > 0;){
+                m.put(line.split("=")[0].trim(), line.split("=")[1].trim());
+            }
+            reader.close();
+            if (m.containsKey("mqtt_host")) {c.setMqttHost(m.get("mqtt_host"));}
+            if (m.containsKey("mqtt_port")) {c.setMqttPort(m.get("mqtt_port"));}
+            if (m.containsKey("mqtt_user")) {c.setMqttUser(m.get("mqtt_user"));}
+            if (m.containsKey("mqtt_pass")) {c.setMqttPass(m.get("mqtt_pass"));}
+            if (m.containsKey("serial")) {c.setSerial(m.get("serial"));}
+            if (m.containsKey("upload_url")) {c.setUploadUrl(m.get("upload_url"));}
+            if (m.containsKey("format")) {c.setFormat(m.get("format"));}
+        } catch (IOException e){}
+        return c;
     }
 
     public void save() {
-
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(CONFIG_PATH));
+            writer.write("mqtt_host=" + this.mqttHost);
+            writer.newLine();
+            writer.write("mqtt_port=" + this.mqttPort);
+            writer.newLine();
+            writer.write("mqtt_user=" + this.mqttUser);
+            writer.newLine();
+            writer.write("mqtt_pass=" + this.mqttPass);
+            writer.newLine();
+            writer.write("serial=" + this.serial);
+            writer.newLine();
+            writer.write("upload_url=" + this.uploadUrl);
+            writer.newLine();
+            writer.write("format=" + this.format);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
